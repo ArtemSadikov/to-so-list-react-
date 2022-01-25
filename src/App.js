145206ebc4c0b1f76de2921104a4ec1635a857b2
CatherineGuseva381708-2 <Marzia240015@gmail.com>
2022-01-25 22:21:13 +0300

    add state

--- src/App.js
+++ src/App.js
@@ -11,14 +11,31 @@ class App extends Component {
       tdItems : tdlData
     }
   }
+  handleChange = id => {
+    const index = this.state.tdItems.map(item => item.id).indexOf(id);
+    this.setState(state =>{
+      let {tdItems} = state;
+      console.log(tdItems[index].completed);
+      if (tdItems[index].completed == true){
+        tdItems[index].completed = false;
+      }else{
+        tdItems[index].completed = true;
+      }
+      // tdItems[index].completed = true;
+      console.log(tdItems[index].completed);
+      console.log(tdItems);
+      return tdItems;
+    })
+  }
   render(){
     const {tdItems} = this.state;
     const taskItems = tdItems.map( item => {
       return(
         <Item
-          key={item.id}
-          descripsion={item.descripsion}
-          completed={item.completed}
+          key = {item.id}
+          descripsion = {item.descripsion}
+          completed = {item.completed}
+          handleChange = { () => {this.handleChange(item.id)} }
         />
       )
     })   
--- src/Item/Item.js
+++ src/Item/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 const Item = props => {
+    const textStyleCompleted = {
+        textDecoration : 'line-through'
+    }
+    const textStyleActive = {
+        textDecoration : 'none'
+    }
+
     return (
         <div className="item">
             <div className="item__title">
-                <p>{props.descripsion}</p>
-                <input type="checkbox" defaultChecked={props.completed}/>
+                <p style={ props.completed === true ? textStyleCompleted : textStyleActive}>{props.descripsion}</p>
+                <input type="checkbox" defaultChecked={props.completed} onChange={props.handleChange}/>
             </div>
             <div className="item__actions">
                 <button>edit</button>
